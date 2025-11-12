7092a821be2dfffd8673ee151e06279bb8253192
SmartX <smartxp2p@gmail.com>
2025-11-12 15:07:13 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+OWNER_ID = int(os.getenv("OWNER_ID", "5712520691"))
+CHANNEL_ID = -1003025900916
+OFFICIAL_WEB = "https://telegramp2p.pro"
+AGENT_USER = "@CriptoJay"
