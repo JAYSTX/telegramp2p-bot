2a7485c14bda80eebaa74f6978b8804df0fdcb2c
SmartX <smartxp2p@gmail.com>
2025-11-12 15:11:40 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID", "5712520691"))
-CHANNEL_ID = -1003025900916
+BOT_TOKEN  = os.getenv("TELEGRAM_BOT_TOKEN")
+OWNER_ID   = int(os.getenv("OWNER_ID", "5712520691"))
+CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1003025900916"))  # ← leído del .env
 OFFICIAL_WEB = "https://telegramp2p.pro"
-AGENT_USER = "@CriptoJay"
+AGENT_USER   = "@CriptoJay"
